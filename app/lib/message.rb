3f5ff6d3238ca61ce18ec8f29d81524c191aa186
ptanghe <pierre.tanghe@arkena.com>
2019-03-29 12:40:38 +0100

    Remove useless error message

--- app/lib/message.rb
+++ app/lib/message.rb
@@ -15,20 +15,8 @@ class Message
     'Missing token'
   end
 
-  def self.unauthorized
-    'Unauthorized request'
-  end
-
   def self.account_created
     'Account created successfully'
-  end
-
-  def self.account_not_created
-    'Account could not be created'
-  end
-
-  def self.expired_token
-    'Sorry, your token has expired. Please login to continue.'
   end
 
   def self.invalid_user
